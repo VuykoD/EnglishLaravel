73baec245bba15803f7b1dd11d5fd4fd1aefdd1a
Dima <vuyko@ukr.net>
2018-07-22 22:32:30 +0300

    fix

--- resources/views/layouts/training.blade.php
+++ resources/views/layouts/training.blade.php
@@ -20,7 +20,6 @@
           </a>
 
           <select class="select" id="select_new">
-            <option>1</option>
             <option>3</option> <option>4</option> <option>5</option> <option>6</option>
             <option>7</option> <option>8</option> <option>9</option> <option>10</option>
             <option>11</option> <option>12</option> 
@@ -182,7 +181,7 @@
           </button>
         </div>
         <div class="span1 border_1"> 
-          <label class="checkbox inline"><input type="checkbox" id="Sentence1_3" value="option1"  disabled>1 новое</label><br>
+          <label class="checkbox inline"><input type="checkbox" id="Sentence1_3" value="option1" checked disabled>1 новое</label><br>
           <label class="checkbox inline "><input type="checkbox" id="Sentence23_3" value="option2">2-6 повт.</label>  
         </div>
 
